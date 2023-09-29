5802ac7c703494dc016f9ebfec7ffb368d0db286
namgaytobden <namgyaltobden@gmail.com>
2023-09-29 12:04:01 +0600

    Updated: tag to id

--- action.sh
+++ action.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Split input variables into arrays
-IFS=" " read -ra TAGS <<< "$TAG"
+IFS=" " read -ra IDS <<< "$ID"
 IFS=" " read -ra VALUES <<< "$VALUE"
 IFS=" " read -ra BLOCKS <<< "$BLOCK"
 
-for i in "${!TAGS[@]}"; do
-  TAG="${TAGS[i]}"
+for i in "${!IDS[@]}"; do
+  ID="${IDS[i]}"
   BLOCK="${BLOCKS[i]}"
   VALUE="${VALUES[i]}"
   
-  grep -rl "$TAG" "$MANIFEST_PATH" | while read -r FILE; do
+  grep -rl "$ID" "$MANIFEST_PATH" | while read -r FILE; do
     yq -i e "$BLOCK = \"$VALUE\"" "$FILE"
   done
 done
