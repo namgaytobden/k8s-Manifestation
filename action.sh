4232133c94f93c0a804519348cee5581fbfa32eb
namgaytobden <namgyaltobden@gmail.com>
2023-09-29 11:55:25 +0600

    Updated: loop values and blocks

--- action.sh
+++ action.sh
@@ -5,9 +5,16 @@ IFS=" " read -ra TAGS <<< "$TAG"
 IFS=" " read -ra VALUES <<< "$VALUE"
 IFS=" " read -ra BLOCKS <<< "$BLOCK"
 
-for TAG in "${TAGS[@]}"; do
+for i in "${!TAGS[@]}"; do
+  TAG="${TAGS[i]}"
+  BLOCK="${BLOCKS[i]}"
+  VALUE="${VALUES[i]}"
+  
   grep -rl "$TAG" "$MANIFEST_PATH" | while read -r FILE; do
-    yq -i e "${BLOCKS[@]} = \"${VALUES[@]}\"" "$FILE"
+    yq -i e "$BLOCK = \"$VALUE\"" "$FILE"
   done
 done
+
+
+
 
